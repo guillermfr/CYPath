0275e9a3aab33245e1ce63a7b26e8e36340d28a2
François <81768287+guillermfr@users.noreply.github.com>
2023-05-20 15:18:11 +0200

    Added Javadoc comments

--- src/enumeration/Color.java
+++ src/enumeration/Color.java
@@ -1,8 +1,24 @@
 package enumeration;
 
+/**
+ * Enum for player colours.
+ * This enum is useful when we want to check things depending on the player.
+ */
 public enum Color {
+    /**
+     * Yellow colour.
+     */
     YELLOW,
+    /**
+     * Blue colour.
+     */
     BLUE,
+    /**
+     * Red colour.
+     */
     RED,
+    /**
+     * Green colour.
+     */
     GREEN
 }
--- src/enumeration/Direction.java
+++ src/enumeration/Direction.java
@@ -1,8 +1,24 @@
 package enumeration;
 
+/**
+ * Enum for directions.
+ * This enum is useful when we check if a move is possible, or if a player can place a barrier.
+ */
 public enum Direction {
+    /**
+     * North direction.
+     */
     NORTH,
+    /**
+     * East direction.
+     */
     EAST,
+    /**
+     * South direction.
+     */
     SOUTH,
+    /**
+     * West direction.
+     */
     WEST
 }
