f1a3fa9b6e204addc88a4ee5a6bbf8ae81a8ff6b
ValentinChanter <valentinsing19@gmail.com>
2023-05-27 00:14:52 +0200

    Added new icon and logo

--- src/main/java/graphicInterface/Home.java
+++ src/main/java/graphicInterface/Home.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import static constant.GraphicInterfaceSizes.SCREEN_HEIGHT;
@@ -36,6 +37,7 @@ public class Home extends Application {
             stage.setTitle("CYPath");
             stage.setResizable(false);
             stage.setScene(scene);
+            stage.getIcons().add(new Image(Home.class.getResourceAsStream("/img/icon.jpg")));
             stage.show();
         }
         catch (Exception e) {
