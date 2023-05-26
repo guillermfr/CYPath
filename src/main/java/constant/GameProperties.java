02c1e1cd2e55fd380ea45ff65251f1e5b2506e3a
ValentinChanter <valentinsing19@gmail.com>
2023-05-26 16:47:46 +0200

    Moved constants

--- src/main/java/constant/GameProperties.java
+++ src/main/java/constant/GameProperties.java
@@ -3,16 +3,16 @@ package constant;
 /**
  * Constant class for the properties of the barriers
  */
-public class BarrierProperties {
+public class GameProperties {
     /**
      * The constructor is useless for this class because we only use it for constants
      */
-    private BarrierProperties(){}
+    private GameProperties(){}
 
     /**
-     * Constant for the size of a barrier. It is counted in GRID_GAPs meaning 2 would give a barrier as big as 2 GRID_GAPs
+     * The size of the board
      */
-    public final static double BARRIER_SIZE = 1.4;
+    public final static int BOARD_SIZE = 9;
 
     /**
      * The maximum amount of barrier players can place
--- src/main/java/constant/GraphicInterfaceSizes.java
+++ src/main/java/constant/GraphicInterfaceSizes.java
@@ -15,6 +15,11 @@ public class GraphicInterfaceSizes {
     public final static int GRID_GAP = 5;
 
     /**
+     * Constant for the size of a barrier. It is counted in GRID_GAPs meaning 2 would give a barrier as big as 2 GRID_GAPs
+     */
+    public final static double BARRIER_SIZE = 1.4;
+
+    /**
      * Set width of the screen
      */
     public final static int SCREEN_WIDTH = 1920;
